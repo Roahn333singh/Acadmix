01ea0b23b375dddb8423c5507d9fdc61293cad71
RohanSingh <121742846+Roahn333singh@users.noreply.github.com>
2023-11-16 18:38:43 +0530

    Academix_1.0

---
+++ public/newscript.js
@@ -0,0 +1,12 @@
+
+document.getElementById('myButton').addEventListener('click', function() {
+    document.getElementById('output').innerHTML = '<iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/32d3bf16-54a7-49f2-9d68-924e96d9652a"></iframe>';
+});   
+
+
+document.getElementById('myButton').addEventListener('click', function() {
+    console.log('Button clicked!');
+        // Rest of your code...
+});
+    
+    
